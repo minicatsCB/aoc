274bcaddc34baaf0bb4761228f97e8902971e969
Maribel Díaz <maria.diaz.ext@ing.com>
2024-12-05 17:13:01 +0100

    feat: solve day 2 part 2

---
+++ 2024/src/day-2-part-2.js
@@ -0,0 +1,82 @@
+import data from "../data/day-2-part-2.json" assert { type: 'json' };
+
+/**
+ * Check if all the numbers in the array are increasing.
+ * @param {number[]} arr - The array of numbers.
+ * @returns {boolean} - True if all numbers are increasing, otherwise false.
+ */
+export function isIncreasing(arr) {
+    return arr.every((curr, i) => {
+        const isFirstNum = i === 0;
+        const prev = arr[i - 1];
+        return isFirstNum || curr > prev;
+    });
+}
+
+/**
+ * Check if all the numbers in the array are decreasing.
+ * @param {number[]} arr - The array of numbers.
+ * @returns {boolean} - True if all numbers are decreasing, otherwise false.
+ */
+export function isDecreasing(arr) {
+    return arr.every((curr, i) => {
+        const isFirstNum = i === 0;
+        const prev = arr[i - 1];
+        return isFirstNum || curr < prev;
+    });
+}
+
+/**
+ * Check if adjacent numbers differ by at least one and at most three.
+ * @param {number[]} arr - The array of numbers.
+ * @returns {boolean} - True if adjacent numbers differ by at least one and at most three, otherwise false.
+ */
+export function hasValidGaps(arr = []) {
+    return arr.every((curr, i) => {
+        const isFirstNum = i === 0;
+        const prev = arr[i - 1];
+        const gap = Math.abs(curr - prev);
+        return isFirstNum || gap >= 1 && gap <= 3;
+    });
+}
+
+/**
+ * Check if an array is "safe".
+ * @param {number[]} arr - The array of numbers.
+ * @returns {boolean} - True if the array is "safe", otherwise false.
+ */
+export function isSafe(arr = []) {
+    return (isIncreasing(arr) || isDecreasing(arr)) && hasValidGaps(arr);
+}
+
+/**
+ * Check if removing a single number from an array makes it "safe".
+ * @param {number[]} arr - The array of numbers.
+ * @returns {boolean} - True if removing a single number makes it "safe", otherwise false.
+ */
+export function canBeMadeSafe(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        const newArr = arr.slice(0, i).concat(arr.slice(i + 1));
+        if (isSafe(newArr)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * Get the total number of "safe" arrays.
+ * @param {number[][]} arrays - The array of arrays of numbers.
+ * @returns {number} - The total number of "safe" arrays.
+ */
+export function countSafeArrays(arrays) {
+    return arrays.reduce((count, arr) => {
+        if (isSafe(arr) || canBeMadeSafe(arr)) {
+            count++;
+        }
+        return count;
+    }, 0);
+}
+
+/* const totalSafeArrays = countSafeArrays(data);
+console.log(totalSafeArrays); */
